d6b28de40e6704784f1c19fc7d450022f806f17a
Shubham Chinda <shubhamchinda07@gmail.com>
2020-01-09 13:50:26 +0530

    result page fix

--- src/container/result/index.js
+++ src/container/result/index.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "antd";
 import { withRouter } from "react-router-dom";
 
-const result = props => {
-  const { name, obtainedMarks, totalMarks } =  props.location && props.location.student;
-// const name = true
+const Result = props => {
+  const [name, setName] = useState(null);
+  const [obtainedMarks, setObtainedMarks] = useState(null);
+  const [totalMarks, setTotalMarks] = useState(null);
+
+  useEffect(() => {
+    console.log(props);
+    if (!props.history.location.student) {
+      props.history.push("/");
+    } else {
+      const {
+        name: n,
+        obtainedMarks: o,
+        totalMarks: t
+      } = props.location.student;
+      setName(n);
+      setObtainedMarks(o);
+      setTotalMarks(t);
+    }
+  }, []);
+
   return (
-    <div style={{ textAlign: "center" , margin: "200px 500px 500px 500px", padding: "50px 100px"}}>
+    <div
+      style={{
+        textAlign: "center",
+        margin: "200px 500px 500px 500px",
+        padding: "50px 100px"
+      }}
+    >
       {name ? (
         <Card
           title={`Result of ${name}`}
@@ -23,4 +47,4 @@ const result = props => {
   );
 };
 
-export default withRouter(result);
+export default withRouter(Result);
